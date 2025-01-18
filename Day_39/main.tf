39a785ded539480b79d2a6118e4f47ffd4195ad9
ajayvbabu <ajayvbabu.mec@gmail.com>
2025-01-18 16:55:38 +0530

    update main.tf to add the provider block to config.tf

--- Day_39/main.tf
+++ Day_39/main.tf
@@ -1,11 +1,16 @@
-provider "aws" {
-  region  = "us-east-1"
-}
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "terraform-basics-bucket"
 
   tags = {
     Name        = "terraform-basics-bucket"
     Environment = "Day 40"
+  }
+}
+resource "aws_s3_bucket" "my_bucket_1" {
+  bucket = "terraform-basics-bucket-1"
+
+  tags = {
+    Name        = "terraform-basics-bucket 1"
+    Environment = "Day 39"
   }
 }
